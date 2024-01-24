c81b741f96d6c31378383923ce0db8d9c616d197
Bas-vE <bas.v.etten@gmail.com>
2024-01-24 23:19:19 +0100

    Replaced the Eventsource for a direct REST API call

--- drivers/slimmelezer/device.js
+++ drivers/slimmelezer/device.js
@@ -1,65 +1,113 @@
 'use strict';
 
 const { Device } = require('homey');
-const EventSource = require('eventsource');
 const { timeEnd } = require('console');
 
 class SlimmeLezerDevice extends Device 
 {
   // Define class fields
   eventsource;
+  meterData = 
+  {
+    PowerConsumed:0,
+    PowerConsumedPhase1:0,
+    PowerConsumedPhase2:0,
+    PowerConsumedPhase3:0,
+    PowerProduced:0,
+    PowerProducedPhase1:0,
+    PowerProducedPhase2:0,
+    PowerProducedPhase3:0,
+    GasConsumed:0,
+    GasConsumedBe:0,
+    EnergyConsumedT1:0,
+    EnergyConsumedT2:0,
+    EnergyConsumedL:0,
+    EnergyProducedT1:0,
+    EnergyProducedT2:0,
+    EnergyProducedL:0,
+    VoltagePhase1:0,
+    VoltagePhase2:0,
+    VoltagePhase3:0,
+    CurrentPhase1:0,
+    CurrentPhase2:0,
+    CurrentPhase3:0
+  };
   
   // onInit is called when the device is initialized.
   async onInit() 
   {
-    var address = this.getData().address;
     var self = this;
-
-    var produced = 0;
-    var consumed = 0;
+    var address = this.getData().address;
+    var apiAddress = 'http://'+ address + '/sensor/';
 
     this.log('SlimmeLezer on address: ' + address + ' has been initialized');
 
-    this.connect(address);
+    setInterval(async () => {
+      self.meterData.PowerConsumed = await this.GetData(apiAddress + 'power_consumed');
+      self.meterData.PowerConsumedPhase1 = await this.GetData(apiAddress + 'power_consumed_phase_1');
+      self.meterData.PowerConsumedPhase2 = await this.GetData(apiAddress + 'power_consumed_phase_2');
+      self.meterData.PowerConsumedPhase3 = await this.GetData(apiAddress + 'power_consumed_phase_3');
+
+      self.meterData.PowerProduced = await this.GetData(apiAddress + 'power_produced');
+      self.meterData.PowerProducedPhase1 = await this.GetData(apiAddress + 'power_produced_phase_1');
+      self.meterData.PowerProducedPhase2 = await this.GetData(apiAddress + 'power_produced_phase_2');
+      self.meterData.PowerProducedPhase3 = await this.GetData(apiAddress + 'power_produced_phase_3');
+
+      self.meterData.GasConsumed = await this.GetData(apiAddress + 'gas_consumed');
+      self.meterData.GasConsumedBe = await this.GetData(apiAddress + 'gas_consumed_belgium');
+
+      self.meterData.EnergyConsumedL = await this.GetData(apiAddress + 'energy_consumed_luxembourg');
+      self.meterData.EnergyConsumedT1 = await this.GetData(apiAddress + 'energy_consumed_tariff_1');
+      self.meterData.EnergyConsumedT2 = await this.GetData(apiAddress + 'energy_consumed_tariff_2');
+
+      self.meterData.EnergyProducedL = await this.GetData(apiAddress + 'energy_produced_luxembourg');
+      self.meterData.EnergyProducedT1 = await this.GetData(apiAddress + 'energy_produced_tariff_1');
+      self.meterData.EnergyProducedT2 = await this.GetData(apiAddress + 'energy_produced_tariff_2');
+
+      self.meterData.VoltagePhase1 = await this.GetData(apiAddress + 'voltage_phase_1');
+      self.meterData.VoltagePhase2 = await this.GetData(apiAddress + 'voltage_phase_2');
+      self.meterData.VoltagePhase3 = await this.GetData(apiAddress + 'voltage_phase_3');
+
+      self.meterData.CurrentPhase1 = await this.GetData(apiAddress + 'current_phase_1');
+      self.meterData.CurrentPhase2 = await this.GetData(apiAddress + 'current_phase_2');
+      self.meterData.CurrentPhase3 = await this.GetData(apiAddress + 'current_phase_3');
+
+      this.UpdateCapabilites();
+    }, 2000);
+
   }
- 
-  // connect is called to start the listener for data from the device
-  connect(address)
+
+  UpdateCapabilites()
   {
-    var self = this;
+    this.setCapabilityValue('measure_power.produced',this.meterData.PowerProduced*1000);
+    this.setCapabilityValue('measure_power.consumed',this.meterData.PowerConsumed*1000);
+    this.setCapabilityValue('measure_power', (this.meterData.PowerConsumed - this.meterData.PowerProduced)*1000);
 
-    var produced = 0;
-    var consumed = 0;
+    this.setCapabilityValue('meter_power.T1',this.meterData.EnergyConsumedT1);
+    this.setCapabilityValue('meter_power.T2',this.meterData.EnergyConsumedT2);
 
-    //create an eventsource that listens to the slimmemeter events
-    this.eventsource = new EventSource('http://' + address + '/events');
-    this.eventsource.addEventListener('state', function (e) 
+    this.setCapabilityValue('meter_gas',this.meterData.GasConsumed);
+  }
+
+  // REST Get for getting the data from the sensor
+  async GetData(address)
+  {
+    try 
     {
-      //parse the json data and only get the parts we need to use
-      var obj = JSON.parse(e.data);
-      switch(obj.id)
+      const res = await fetch(address);    
+      var data = await res.json();
+      
+      if(data.value == null)
       {
-        case 'sensor-power_consumed':
-          consumed = obj.value*1000
-          self.setCapabilityValue('measure_power.consumed',consumed);
-          self.setCapabilityValue('measure_power', consumed - produced);
-          break;
-        case 'sensor-power_produced':
-          produced = obj.value*1000;
-          self.setCapabilityValue('measure_power.produced',produced);
-          self.setCapabilityValue('measure_power', consumed - produced);
-          break;
-        case 'sensor-energy_consumed_tariff_1':
-          self.setCapabilityValue('meter_power.T1',obj.value);
-          break;
-        case 'sensor-energy_consumed_tariff_2':
-          self.setCapabilityValue('meter_power.T2',obj.value);
-          break;
-        case 'sensor-gas_consumed':
-          self.setCapabilityValue('meter_gas',obj.value);
-          break;
+        return 0;
       }
-    })    
+
+      return data.value;
+    } 
+    catch (err) 
+    {
+      console.log(err.message); //can be console.error
+    }
   }
 
   /**
